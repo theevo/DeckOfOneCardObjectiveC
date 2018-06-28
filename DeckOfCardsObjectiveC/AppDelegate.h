e26526d1d0aae5cc288e3e9462f569132b194c94
Nick Reichard <reich452@regis.edu>
2018-06-28 13:54:17 -0600

    first commit

---
+++ DeckOfCardsObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckOfCardsObjectiveC
+//
+//  Created by Nick Reichard on 6/22/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
